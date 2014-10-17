c4615efbeebda71da353d9105f22713219437d4b
egabczo <gabor.czompo@ericsson.com>
2014-10-17 15:30:07 +0200

    SectionSelector
    Change-Id: I769886107ba22f1c17587253b86c2653de7aa674

--- ConfigTools/src/SectionSelector.java
+++ ConfigTools/src/SectionSelector.java
@@ -6,11 +6,14 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JCheckBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 
 
 public class SectionSelector extends JDialog {
-
-	private final JPanel contentPanel = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -32,14 +35,6 @@ public class SectionSelector extends JDialog {
 		setTitle("Section selector");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		{
-			JCheckBox chckbxSection = new JCheckBox("Section 5");
-			chckbxSection.setBounds(8, 46, 120, 23);
-			contentPanel.add(chckbxSection);
-		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -56,6 +51,39 @@ public class SectionSelector extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 1, 0, 10));
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		panel_1.add(btnNewButton_3);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		panel_2.setLayout(new GridLayout(0, 1, 0, 10));
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_2.add(btnNewButton_1);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		panel_2.add(btnNewButton_4);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3);
+		panel_3.setLayout(new GridLayout(0, 1, 0, 10));
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel_3.add(btnNewButton_2);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		panel_3.add(btnNewButton_5);
 	}
-
 }
