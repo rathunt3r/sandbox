4119a6e2dc81bdafe97b070a3dbc88d64ec5b37e
egabczo <gabor.czompo@ericsson.com>
2014-10-29 15:58:45 +0100

    Change-Id: I56d87553bb5e12a3bf15f1946ca188d36e46e210

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,4 +1,6 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,16 +13,67 @@ public class Test {
 				"/opt/mirror/loadingGroup01_1 is kept open by: zxc (PID: 99, UID: 0).\n" +
 				"/opt/mirror/loadingGroup01_1 iD: 0).\n" +
 				"/opt/mirror/adm/etc is kept open by: named (PID: 29487, UID: 0).\n";
-		check(log);
+		//check(log);
 		
 		String log2 = "Remove /etc/save_ioconfig.conf from the /etc/save_ioconfig.conf config file, it is not allowed to be saved \n" +
 				"Remove /etc/securetty from the /etc/save_ioconfig.conf config file, it is not allowed to be saved \n" +
 				"Remove /etc/rsyslog.conf from the /etc/save_ioconfig.conf config file, it is not allowed to be saved \n" +
 				"Remove /etc/sysctl.conf from the /etc/save_ioconfig.conf config file, it is not allowed to be saved \n";
-		check2(log2);
+		//check2(log2);
+		
+		//dateDiff();
+		
+		isEmptyOrNullPrintout();
+	}
+	
+	private static void isEmptyOrNullPrintout(){
+		Date date = new Date();
+		String string1 = null;
+		String string2 = "string2";
 		
 		
+		System.out.println("Alarm found which meet the given condition(s): " + 
+				(date == null ? "" : date.toString()) + 
+				(isEmptyOrNull(string1) ? "" : string1) +
+				(isEmptyOrNull(string2) ? "" : string2)
+		);
+	}
+	
+	private static boolean isEmptyOrNull(String string){
+		if (string == null || string.length() == 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//2014.10.29. date difference between two dates
+	private static void dateDiff(){
 		
+		String dateStart = "2014-10-29 09:29:58";
+		String dateStop = "2014-10-31 10:31:48";
+	 
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	 
+		Date d2 = null;
+		Date d1 = null;
+	 
+		try {
+			d1 = format.parse(dateStart);
+			d2 = format.parse(dateStop);
+	 
+			//DateTime dt1 = new DateTime(d1);
+			//DateTime dt2 = new DateTime(d2);
+			
+			
+			System.out.print((d2.getTime() - d1.getTime()) / (24 * 60 * 60 * 1000));
+			//System.out.print(Hours.hoursBetween(dt1, dt2).getHours() % 24 + " hours, ");
+			//System.out.print(Minutes.minutesBetween(dt1, dt2).getMinutes() % 60 + " minutes, ");
+			//System.out.print(Seconds.secondsBetween(dt1, dt2).getSeconds() % 60 + " seconds.");
+	 
+		 } catch (Exception e) {
+			e.printStackTrace();
+		 }
 	}
 	
 	private static void check2(String response){
